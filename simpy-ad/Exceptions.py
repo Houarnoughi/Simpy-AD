a638284b3edc966b942b7cfcecaf083b85bf7a92
emmzak <emmanuelzakaryan@gmail.com>
2022-04-30 16:22:42 +0200

    add exceptions

---
+++ simpy-ad/Exceptions.py
@@ -0,0 +1,13 @@
+
+
+"""
+when assiging a task to a pu
+"""
+class OutOfMemoryException(Exception):
+    pass
+
+"""
+when scheduler has no more tasks
+"""
+class NoMoreTasksException(Exception):
+    pass
