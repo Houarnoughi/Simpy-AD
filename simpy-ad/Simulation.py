70a0c3e0e491791f190c4d0af1f0d03bfb4d9388
emmzak <emmanuelzakaryan@gmail.com>
2022-04-30 16:24:18 +0200

    init entities from config file

--- simpy-ad/Simulation.py
+++ simpy-ad/Simulation.py
@@ -29,23 +29,19 @@ from Server import Server
 from CNNModel import CNNModel
 from Task import Task
 from TaskMapper import TaskMapper
-from Colors import END, GREEN, YELLOW, RED
+from Colors import END, GREEN, YELLOW, RED, BLUE
 from TaskCriticality import TaskCriticality
 from Store import Store
 from Plotter import Plotter
 import config
+import os
+import random
 
 env = simpy.Environment()
 
 """
-Vehicle init
+vehicle task types
 """
-start = Location("Gare VA", 50.36328322047431, 3.5171747551323005)
-final = Location("Gare Lille", 50.63725143907785, 3.0702985651377745)
-## PU init
-pu1 = AGX(task_list=[], scheduler=RoundRobinSchedulingPolicy(config.QUANTUM), env=env)
-Store.addPU(pu1)
-
 inception = CNNModel('Inception-v3', 1024)
 resnet18 = CNNModel('ResNet-18', 480)
 mobilenet = CNNModel('MobileNet0.25-v1', 240)
@@ -55,41 +51,73 @@ vehicle_tasks = [
     Task(resnet18.getModelFLOPS(), resnet18.getModelMemory(), criticality=TaskCriticality.MEDIUM),
     Task(mobilenet.getModelFLOPS(), mobilenet.getModelMemory(), criticality=TaskCriticality.LOW),
 ]
-vehicle = Vehicle(
-    c_location=start, 
-    f_location=final, 
-    speed=10, 
-    bw = 10e6,
-    task_list=vehicle_tasks, PU_list=[pu1], required_FPS=config.FPS, env=env)
 """
-RSU init
+Vehicle init x10
+
+Lille triangle limits
+
+50.650796308194764, 3.037816505423616 - 50.649129252863915, 3.0776296527024805
+                
+                
+50.63089852336916, 3.032710061899567 - 50.631246374330935, 3.08480585127184
 """
-location = Location("", 50, 45)
-# PU init
-pu2 = TeslaV100(task_list=[], scheduler=RoundRobinSchedulingPolicy(config.QUANTUM), env=env)
-Store.addPU(pu2)
 
-pu3 = TeslaV100(task_list=[], scheduler=RoundRobinSchedulingPolicy(config.QUANTUM), env=env)
-Store.addPU(pu3)
+for _ in range(config.VEHICLES):
+    ## PU init
+    pu1 = AGX(task_list=[], scheduler=RoundRobinSchedulingPolicy(config.QUANTUM), env=env)
+    Store.addPU(pu1)
 
-## Server init
-server = Server(pu_list=[pu2, pu3], bw=1, env=env)
+    vehicle = Vehicle(
+        c_location=Location("random", random.uniform(config.MIN_LAT, config.MAX_LAT), random.uniform(config.MIN_LONG, config.MAX_LONG)), 
+        f_location=Location("Gare Lille", 50.63725143907785, 3.0702985651377745), 
+        speed=10, 
+        bw = 10e6,
+        task_list=vehicle_tasks, 
+        PU_list=[pu1], 
+        required_FPS=config.FPS, 
+        env=env)
+    vehicle.showInfo()
+    Store.vehicle_list.append(vehicle)
+"""
+RSU init  x3
+"""
+locations = [
+    Location("Zoo de Lille", 50.64099393427632, 3.044548801247785),
+    Location("Jardin des Géants", 50.64347018158827, 3.0806523044990617),
+    Location("Palais bx arts", 50.63222755233801, 3.0628195821035655),
+    Location("Moulins", 50.620905648844506, 3.06973893974428),
+    Location("wazemmes", 50.627218409442975, 3.0400339217074266)
+]
+for loc in locations:
 
-rsu = RoadSideUnit(activity_range=100, location=location, server_list=[server], to_vehicle_bw=1, to_cloud_bw=1, env=env)
+    pu = TeslaV100(task_list=[], scheduler=RoundRobinSchedulingPolicy(config.QUANTUM), env=env)
+    Store.addPU(pu)
 
-vehicle.showInfo()
-rsu.showInfo()
+    rsu = RoadSideUnit(
+        activity_range=100, 
+        location=loc, 
+        server_list=[
+            Server(pu_list=[pu], bw=1, env=env)
+        ], 
+        to_vehicle_bw=1, to_cloud_bw=1, env=env)
+    rsu.showInfo()
+    Store.rsu_list.append(rsu)
+
 
 Store.showPUs()
 
 taskMapper = TaskMapper(env)
 
-SIM_TIME = 10 #10**1
 print("Enter to start Simulation")
 input()
 
-#p = Plotter(env)
+p = Plotter(env)
 
-env.run(until=SIM_TIME)
+env.run(until=config.SIM_TIME)
 
 Store.showStats()
+Store.export()
+
+# task: Task = None
+# for task in Store.all_tasks:
+#     print(task.id, task.execution_start_time, task.execution_end_time, task.remaining_flop)
