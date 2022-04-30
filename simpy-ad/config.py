1a217294fd2fdbb3151a9d6d914eb9811651ece2
emmzak <emmanuelzakaryan@gmail.com>
2022-04-30 20:30:16 +0200

    add offload options

--- simpy-ad/config.py
+++ simpy-ad/config.py
@@ -3,21 +3,27 @@ Global variables
 """
 import os
 
-# scheduler
-QUANTUM = 0.01
-
-# vehicle
-VEHICLES = 1
-
-FPS = 30
-N_CLOSEST_PU = 5
-
-## area
+## simulation area
 MIN_LAT, MAX_LAT = 50.63089852336916, 50.650796308194764
 MIN_LONG, MAX_LONG = 3.032710061899567, 3.08480585127184
 
-# task mapper
+# vehicle
+VEHICLES = 10
+FPS = 30 
+N_CLOSEST_PU = 5
+OFFLOAD = True
+OFFLOAD_TO_VEHICLE = True
+OFFLOAD_TO_RSU= True
+OFFLOAD_TO_DATACENTER = False
+
+# task mapper, task to PU attribution
 RANDOM = True
+
+# steps
+SIM_TIME = 1
+
+# scheduler
+QUANTUM = 0.01
 
 # export results
 OUT_FILE_PATH = f'output/results.csv'
@@ -25,5 +31,3 @@ OUT_FILE_PATH = f'output/results.csv'
 # Simulation Mode
 DATA_GENERATION_MODE = False
 
-# steps
-SIM_TIME = 1
