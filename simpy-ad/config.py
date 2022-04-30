393ca254f772ab06fb456efe7fd937c2d78e364b
emmzak <emmanuelzakaryan@gmail.com>
2022-04-30 16:23:59 +0200

    new config values

--- simpy-ad/config.py
+++ simpy-ad/config.py
@@ -1,12 +1,29 @@
 """
 Global variables
 """
+import os
 
 # scheduler
-QUANTUM = 0.005
+QUANTUM = 0.01
 
 # vehicle
+VEHICLES = 1
+
 FPS = 30
+N_CLOSEST_PU = 5
+
+## area
+MIN_LAT, MAX_LAT = 50.63089852336916, 50.650796308194764
+MIN_LONG, MAX_LONG = 3.032710061899567, 3.08480585127184
 
 # task mapper
 RANDOM = True
+
+# export results
+OUT_FILE_PATH = f'output/results.csv'
+
+# Simulation Mode
+DATA_GENERATION_MODE = False
+
+# steps
+SIM_TIME = 1
