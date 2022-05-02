cccd33d61496b4ea1f35d66b52b2d4cf1837f1d2
emmzak <emmanuelzakaryan@gmail.com>
2022-05-02 15:51:27 +0200

    plot locations

--- simpy-ad/Plotter.py
+++ simpy-ad/Plotter.py
@@ -1,39 +1,76 @@
+from turtle import color
 import numpy as np
 from collections import namedtuple
 import random
+from Store import Store
 import matplotlib.pyplot as plt
+import config
+from Vehicle import Vehicle
 
+class Plotter:
+    def __init__(self, env) -> None:
+        self.env = env
+        #self.env.process(self.run())
+        self.env.process(self.map())
 
-Location = namedtuple('Location', ['lon','lat'])
+    def map(self):
+        
+        plt.ion()
+        plt.figure(dpi=150)  
+        plt.axis([config.MIN_LAT, config.MAX_LAT, config.MIN_LONG, config.MAX_LONG])
+        #plt.axis([1, 5, 1, 10])
+        ax = plt.gca()
+        #ax.set_xlim([config.MIN_LAT, config.MAX_LAT])
+        #ax.set_ylim([config.MIN_LONG, config.MAX_LONG])
+        plt.title("Position")
+        plt.xlabel("Type")
+        plt.ylabel("Vehicle position")
 
-va = Location(50.36328322047431, 3.5171747551323005)
-lille = Location(50.63725143907785, 3.0702985651377745)
+        while True:
+            plt.clf()
+            # draw RSU
+            for rsu in Store.rsu_list:
+                lat = rsu.getLocation().getLatitude() 
+                long = rsu.getLocation().getLongitude() 
+                plt.scatter(long, lat, color="green", s=100)
+                plt.annotate(rsu.name, (long, lat))
 
-step = 10
-deviation = 20
+            # draw vahicles
+            for v in Store.vehicle_list:
+                lat = v.getLocation().getLatitude() 
+                long = v.getLocation().getLongitude() 
+                plt.scatter(long, lat, color="red")
 
-trip_lon = np.linspace(va.lon, lille.lon, step)
-trip_lat = np.linspace(va.lat, lille.lat, step)
+            plt.pause(0.00005)
+            
+            yield self.env.timeout(1)
+            
 
-augm = lambda e: e + np.random.uniform(-1,1)/deviation
-trip_lon = list(map(augm, trip_lon))
-trip_lat = list(map(augm, trip_lat))
+    def run(self):
+        plt.ion()
+        plt.figure(dpi=150)  
+        ax = plt.gca()
+        #ax.set_xlim([0, config.SIM_TIME])
+        #ax.set_ylim([0, 100000])
+        plt.title("Tasks")
+        plt.xlabel("Type")
+        plt.ylabel("Task count")
 
-plt.figure(dpi=150)  
+        x = 0
+        while True:
+            x=x+1
+            #plt.scatter(x, count)
 
-plt.scatter(*va)
-plt.annotate("Va", (va.lon, va.lat))
-plt.scatter(*lille)
-plt.annotate("Lille", (lille.lon, lille.lat))
+            try:
+                plt.bar("Success", Store.getSuccessTaskCount(), color='green')
+                #plt.bar("Started Failed", Store.getStartedFailedTaskCount(), color='yellow')
+                #plt.bar("Not finished", Store.getStartedNotFinishedTaskCount(), color='orange')
+                #plt.bar("Not started", Store.getNotStartedTaskCount(), color='red')
 
-plt.plot(trip_lon,trip_lat,"go--",label='Trip',linestyle="dashed", markersize=2, linewidth=5)
+                plt.pause(0.00005)
+            except Exception as e:
+                print(e)
+                input()
+                pass
 
-plt.gca().invert_xaxis()
-plt.gca().invert_yaxis()
-
-plt.xlabel('Longitude')
-plt.ylabel('Latitude')
-plt.title(" Trip ")
-
-plt.legend()
-plt.show()
+            yield self.env.timeout(0.1)
