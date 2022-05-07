d34bf80e501847cde5cb53cf99f16a64c7dd46b2
emmzak <emmanuelzakaryan@gmail.com>
2022-05-07 23:24:19 +0200

    add new Task implementation

--- simpy-ad/Task.py
+++ simpy-ad/Task.py
@@ -1,11 +1,13 @@
-from ProcessingUnit import ProcessingUnit
 from TaskCriticality import TaskCriticality
 from typing import TYPE_CHECKING
+from enum import Enum
+from abc import ABC, abstractmethod
 
 if TYPE_CHECKING:
     from Vehicle import Vehicle
+    from ProcessingUnit import ProcessingUnit
 
-class Task(object):
+class TaskStatus(Enum):
     CREATED = 0
     READY = 1
     QUEUED = 2
@@ -17,9 +19,121 @@ class Task(object):
     RESUMED = 8
     FAILED_RESOURCE_UNAVAILABLE = 9
 
+class _Task(ABC):
+    """
+    Abstract class for defining tasks
+
+    Task 
+        - is created on a vehicle
+        - is assigned to a PU to be executed
+        - flop is the amount of computation
+        - remaining_flop is the amount to execute, modified by PU
+        - size is task's memory footprint
+        - execution_start_time is Simpy time of when PU started executing the task
+        - execution_end_time is Simpy time set when there's no more remaining_flop to execute
+        - scheduler_rounds is the number of times a task's been through the TaskScheduler (Round Robin)
+    """
     idx = 0
 
-    def __init__(self, flop, size, criticality: TaskCriticality=None, preemptive=False, real_time=False, status=PAUSED, currentVehicle=None,currentPU=None):
+    def __init__(self, vehicle: 'Vehicle' = None, deadline=0, pu: 'ProcessingUnit' = None, flop: float = 0, size: float = 0):
+        self.id = _Task.idx
+        self.name = f'Task-{self.id}'
+        _Task.idx += 1
+
+        # parent
+        self.vehicle: 'Vehicle' = vehicle
+
+        self.status: TaskStatus = None
+        self.pu: 'ProcessingUnit' = pu
+
+        # props
+        self.flop: float = flop
+        self.remaining_flop: float = flop
+        self.size: float = size
+
+        # execution 
+        self.deadline: float = deadline
+        self.execution_start_time: float = -1
+        self.execution_end_time: float = -1
+
+        # scheduler
+        self.scheduler_rounds = 0
+
+    def setVehicle(self, vehicle: 'Vehicle'):
+        self.vehicle = vehicle
+
+    def getVehicle(self) -> 'Vehicle':
+        return self.vehicle
+
+    def setFlop(self, flop: float):
+        self.flop = flop
+
+    def getFlop(self) -> float:
+        return self.flop
+
+    def setRemainingFlop(self, flop: float):
+        self.flop = flop
+
+    def decreaseRemainingFlop(self, flop: float):
+        self.remaining_flop -= flop
+
+    def getRemainingFlop(self) -> float:
+        return self.remaining_flop
+
+    def setSize(self, size: float):
+        self.size = size
+
+    def getSize(self) -> float:
+        return self.size
+
+    def setDeadline(self, deadline: float):
+        self.deadline = deadline
+
+    def getDeadline(self) -> float:
+        return self.deadline
+
+    def setStatus(self, status: TaskStatus):
+        self.status = status
+
+    def getStatus(self) -> TaskStatus:
+        return self.status
+
+    def setPu(self, pu: 'ProcessingUnit'):
+        self.pu = pu
+
+    def getPu(self) -> 'ProcessingUnit':
+        return self.pu
+
+    def setExecutionStartTime(self, time: float):
+        self.execution_start_time = time
+
+    def getExecutionStartTime(self) -> float:
+        return self.execution_start_time
+
+    def setExecutionEndTime(self, time: float):
+        self.execution_end_time = time
+
+    def getExecutionEndTime(self) -> float:
+        return self.execution_end_time
+
+    def addSchedulerRound(self):
+        self.scheduler_rounds += 1
+
+    def getSchedulerRounds(self) -> int:
+        return self.scheduler_rounds
+
+    def getInfos(self) -> str:
+        return f'[{self.name}, flop={self.flop}, remainingFlop={self.remaining_flop}, size={self.size}, startTime={self.execution_start_time}, endTime={self.execution_end_time}, pu={self.pu}, vehicle={self.vehicle}, status={self.status}, rounds={self.scheduler_rounds}]'
+
+    def __repr__(self) -> str:
+        return f'[{self.name}, {self.pu}]'
+
+
+class Task(object):
+
+    idx = 0
+
+    def __init__(self, flop, size, criticality: TaskCriticality = None, preemptive=False, real_time=False, status=TaskStatus.PAUSED, currentVehicle=None, currentPU=None):
         self.id = Task.idx
         self.name = f'Task-{Task.idx}'
         Task.idx += 1
@@ -40,7 +154,7 @@ class Task(object):
         self.execution_end_time = -1
 
         self.scheduler_rounds = 0
-    
+
     def isSuccess(self):
         return self.isStarted() and self.isFinished() and self.isFinishedBeforeDeadline()
 
@@ -49,13 +163,13 @@ class Task(object):
 
     def isIncomplete(self):
         return self.isStarted() and not self.isFinished()
-    
+
     def isStarted(self):
         return self.execution_start_time != -1
-    
+
     def isFinished(self):
         return self.execution_end_time != -1
-    
+
     def isFinishedBeforeDeadline(self):
         return self.execution_end_time < self.deadline
 
@@ -92,10 +206,10 @@ class Task(object):
     def setStatus(self, status):
         self.status = status
 
-    def getCurrentPU(self) -> ProcessingUnit:
+    def getCurrentPU(self) -> 'ProcessingUnit':
         return self.currentPU
 
-    def setCurrentPU(self, pu: ProcessingUnit):
+    def setCurrentPU(self, pu: 'ProcessingUnit'):
         self.currentPU = pu
 
     def getCurrentVehicle(self) -> 'Vehicle':
@@ -106,28 +220,34 @@ class Task(object):
 
     # OS time
     def getTotalExecutionTime(self):
-        #return self.total_execution_time
+        # return self.total_execution_time
         return self.execution_end_time - self.execution_start_time
-    
+
     def updateTotalExecutionTime(self, time):
         self.total_execution_time = self.getTotalExecutionTime() + time
-    
+
     # Simpy time
     def setDeadline(self, deadline):
         self.deadline = deadline
-    
+
     def getDeadline(self):
         return self.deadline
 
     # Simpy time
     def setExpectedExecTime(self, time):
         self.expected_exec_time = time
-    
+
     def getExpectedExecTime(self):
         return self.expected_exec_time
 
     def __str__(self):
         return f"[ID: {self.id}, {self.name}]"
-    
+
     def __repr__(self) -> str:
         return f"[ID: {self.id}, {self.name}]"
+
+# if __name__ == '__main__':
+#     tasks = [_Task() for i in range(5)]
+#     t = tasks[0]
+#     infos = t.getInfos()
+#     print(infos)
